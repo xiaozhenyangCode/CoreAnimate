985be7533ffe7ffef43c601c30e31b319671dc62
heiyanmini <xiaozhenyangcode@163.com>
2020-09-28 14:15:15 +0800

    定时帧

--- CoreAnimate/Base/BaseTableViewController.swift
+++ CoreAnimate/Base/BaseTableViewController.swift
@@ -16,9 +16,9 @@ class BaseTableViewController: UITableViewController {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
         textLabelString = ["视觉效果", "变换", "专用图层", "隐式动画",
-                           "显式动画", "图层时间", "缓冲"]
+                           "显式动画", "图层时间", "缓冲", "基于定时器的动画"]
         classNames = ["CoreAnimateTableViewController", "TransFormTableViewController", "SpecialLayerTableViewController", "ImplicitAnimationTableViewController"
-                      , "ExplicitAnimationTableViewController", "LayerTimeTableViewController", "BufferBaseTableViewController"]
+                      , "ExplicitAnimationTableViewController", "LayerTimeTableViewController", "BufferBaseTableViewController", "AnimationTimerTableViewController"]
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
---
+++ CoreAnimate/基于定时器的动画/AnimationTimerTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AnimationTimerTableViewController.swift
+//  CoreAnimate
+//
+//  Created by heiyanmini on 2020/9/28.
+//  Copyright © 2020 heiyanmacmini. All rights reserved.
+//
+
+import UIKit
+
+class AnimationTimerTableViewController: BaseTableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textLabelString = ["定时帧"]
+
+        classNames = ["TimeFrameViewController"]
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textLabelString.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        cell.textLabel?.text = textLabelString[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let ctrl = classFromString(classNames[indexPath.row]) else { return }
+        navigationController?.pushViewController(ctrl, animated: true)
+    }
+}
