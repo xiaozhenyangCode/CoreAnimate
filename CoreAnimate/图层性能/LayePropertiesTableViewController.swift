4ea6d3028683d75540c2ec8863f342c0a975566a
heiyanmini <xiaozhenyangcode@163.com>
2020-10-13 14:22:09 +0800

    减少图层数量

--- CoreAnimate/图层性能/LayePropertiesTableViewController.swift
+++ CoreAnimate/图层性能/LayePropertiesTableViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class LayePropertiesTableViewController: BaseTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        textLabelString = ["隐式绘制", "离屏渲染", "混合和过度绘制"]
+        textLabelString = ["隐式绘制", "离屏渲染", "混合和过度绘制", "减少图层数量"]
 
-        classNames = ["ImplicitMappingViewController", "OffScreenRenderingViewController", "MixAndOverdrawViewController"]
+        classNames = ["ImplicitMappingViewController", "OffScreenRenderingViewController", "MixAndOverdrawViewController", "ReduceNumberLayersViewController"]
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
